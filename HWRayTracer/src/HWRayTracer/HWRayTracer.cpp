8b82f0915faa9afaa722346c5ee4df1b1225cb92
李红伟 <321152405@qq.com>
2021-08-12 20:58:14 +0800

    add brdf

--- HWRayTracer/src/HWRayTracer/HWRayTracer.cpp
+++ HWRayTracer/src/HWRayTracer/HWRayTracer.cpp
@@ -15,17 +15,23 @@ color ray_color( ray& r,Sence s,int max_depth) {
 	if (max_depth < 1)return color(0.0);
 
 	intersection inter = intersection();
+	//befor hit scatter event
+	//TODO
+
+
 	if (s.hit(r, 0.001, INFINITY, inter)) {
 		ray& incident_ray = ray();
 		color& attenuation = color();
 		if (inter.materail->scatter(r, inter, incident_ray, attenuation)) {
+			
 			return array_multiply(attenuation,ray_color(incident_ray,s,max_depth-1));
 		}
 		return color(0);
 	}
 
-	auto t = 0.5 * r.direction().y() + 0.5;
-	color res = color(0.52 + t * 0.43, 0.91 + t * 0.07, 1);
+	//auto t = 0.5 * r.direction().y() + 0.5;
+	//color res = color(0.52 + t * 0.43, 0.91 + t * 0.07, 1);
+	color res = color(1);
 	return res;
 }
 
@@ -38,22 +44,22 @@ int main()
 	//_____________________________________render part____________________________________________//
 	//Sence
 	Sence sence = Sence();
-	auto diffuse0 = Diffuse(color(0.8));
+	auto diffuse0 = LambertianDiffuse(color(0.8));
 	auto mirror = Mirror(color(0.95));
-	auto diffuse2 = Diffuse(color(0.2, 0.5, 0.5));
-	auto diffuse3 = Diffuse(color(0.8, 0.5, 0.5));
+	auto diffuse2 = LambertianDiffuse(color(0.2, 0.5, 0.5));
+	auto diffuse3 = LambertianDiffuse(color(0.8, 0.5, 0.5));
 	auto matel1 = Metal(color(1,0.8,1), 0.3);
 	auto matel2 = Metal(color(1,1,0.8), 0.3);
 	auto glass = Glass();
 
 
 	Sphere s1 = Sphere(point3(0,0.5, -5), 1,make_shared<Mirror>(mirror));
-	Sphere s2 = Sphere(point3(2, 0, -0.8), 0.5, make_shared<Diffuse>(diffuse2));
+	Sphere s2 = Sphere(point3(2, 0, -0.8), 0.5, make_shared<LambertianDiffuse>(diffuse2));
 	Sphere s4 = Sphere(point3(0.8, 0, -1.5), 0.5, make_shared<Metal>(matel1));
 	Sphere s5 = Sphere(point3(-0.7, 0, -1.1), 0.5, make_shared<Metal>(matel2));
 	Sphere s6 = Sphere(point3(0.2, -0.1, -0.6), 0.4, make_shared<Glass>(glass));
-	Sphere s3 = Sphere(point3(-2, 0, -1.2), 0.5, make_shared<Diffuse>(diffuse3));
-	Sphere s0 = Sphere(point3(0, -100.5, -1), 100,make_shared<Diffuse>(diffuse0));
+	Sphere s3 = Sphere(point3(-2, 0, -1.2), 0.5, make_shared<LambertianDiffuse>(diffuse3));
+	Sphere s0 = Sphere(point3(0, -100.5, -1), 100,make_shared<LambertianDiffuse>(diffuse0));
 	sence.add(make_shared<Sphere>(s1));
 	sence.add(make_shared<Sphere>(s2));
 	sence.add(make_shared<Sphere>(s3));
@@ -67,12 +73,12 @@ int main()
 
 	//Image
 	const double aspect = camera.getaspect();
-	const int image_width = 1980;
+	const int image_width = 400;
 	const int image_height =static_cast<int> (image_width / aspect);
 	
 	//Render
-	int antialiasing_sample_nums = 100;
-	int ray_trace_maxdepth = 50;
+	int antialiasing_sample_nums = 20;
+	int ray_trace_maxdepth = 10;
 
 	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 	for (int i = image_height ; i >= 0;i--) {
--- HWRayTracer/src/HWRayTracer/Materail.h
+++ HWRayTracer/src/HWRayTracer/Materail.h
@@ -10,23 +10,35 @@ class Materail
 {
 public:
 	virtual bool scatter( const ray& r_out,const intersection& inter, ray& incident_ray,color&  attenuation) const = 0;
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const = 0;
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const = 0;
 };
 
 
-class Diffuse : public Materail {
+class LambertianDiffuse : public Materail {
 public:
 	color albedo;
+	double kd;
 public:
-	Diffuse(vec3 _albedo = vec3(0.5)) :albedo(_albedo) {};
+	LambertianDiffuse(vec3 _albedo = vec3(0.5),double _kd= 0.91) :albedo(_albedo),kd(_kd) {};
 
 	virtual bool scatter(const ray& r_out, const intersection& inter, ray& incident_ray, color& attenuation) const override {
 		vec3 n = inter.normal;
 		point3 incident_ray_direction = random_in_unit_hemisphere(n);
-
 		incident_ray = ray(inter.p, incident_ray_direction);
 		attenuation = albedo;
-
+		auto cos_theta = dot(incident_ray.direction(),n);
+		double eval = get_bsdf(incident_ray.direction(), r_out.direction(), n);
+		double pdf = get_pdf(incident_ray.direction(), r_out.direction(), n);
+		attenuation *= cos_theta * eval * (1 / pdf);
 		return true;
+	}
+
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const override {
+		return 1 / (2 * pi);
+	}
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const override {
+		return kd / pi;
 	}
 };
 
@@ -41,18 +53,27 @@ public:
 		point3 incident_ray_direction = reflect(r_out.direction(), n);
 		incident_ray = ray(inter.p, incident_ray_direction);
 		attenuation = albedo;
-
+		auto cos_theta = 1;
+		double eval = get_bsdf(incident_ray.direction(), r_out.direction(), n);
+		double pdf = get_pdf(incident_ray.direction(), r_out.direction(), n);
+		attenuation *= cos_theta * eval * (1 / pdf);
 		return true;
 	}
-
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const override {
+		return 1;
+	}
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const override {
+		return 1;
+	}
 };
 
 class Metal:public Materail {
 public:
 	color albedo;
 	double fuzz;
+	double kd;
 public:
-	Metal(vec3 _albedo = vec3(1),double _fuzz=0.5f) :albedo(_albedo),fuzz(_fuzz) {};
+	Metal(vec3 _albedo = vec3(1),double _fuzz=0.5f,double _kd =0.55 ) :albedo(_albedo),fuzz(_fuzz),kd(_kd) {};
 
 	virtual bool scatter(const ray& r_out, const intersection& inter, ray& incident_ray, color& attenuation) const override {
 		vec3 n = inter.normal;
@@ -60,9 +81,20 @@ public:
 		incident_ray = ray(inter.p, incident_ray_direction);
 		attenuation = albedo;
 
-		return true;
-	}
+		auto cos_theta = dot(incident_ray.direction(), n);
+		double eval = get_bsdf(incident_ray.direction(), r_out.direction(), n);
+		double pdf = get_pdf(incident_ray.direction(), r_out.direction(), n);
+		//attenuation *= cos_theta * eval * (1 / pdf);
 
+		return true;
+
+	}
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const override {
+		return 1 / (2 * pi);
+	}
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const override {
+		return kd / pi;
+	}
 };
 
 class Glass : public Materail{
@@ -77,11 +109,46 @@ public:
 	virtual bool scatter(const ray& r_out, const intersection& inter, ray& incident_ray, color& attenuation) const override {
 		vec3 n = inter.normal;
 		double eta_to_etap = inter.out_face ? (eta0 / eta1) : (eta1 / eta0);
+
 		point3 incident_ray_target = refract(r_out.direction(), n, eta_to_etap);
 		incident_ray = ray(inter.p, incident_ray_target);
 		attenuation = vec3(1);
 
 		return true;
+	}
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const override {
+		return 1 / (2 * pi);
+	}
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const override {
+		return 1;
+	}
+};
+
+
+class HG_materail : public Materail {
+public:
+	double attenuation;
+	double HG_g;
+
+public:
+	HG_materail(double _attenuation = 1., double _HG_g = 0.5) :attenuation(_attenuation), HG_g(_HG_g) {};
+
+	virtual bool scatter(const ray& r_out, const intersection& inter, ray& incident_ray, color& attenuation) const override {
+		vec3 n = inter.normal;
+		point3 incident_ray_target = random_in_unit_sphere();
+		incident_ray = ray(inter.p, incident_ray_target);
+		auto cos_theta = dot(incident_ray.direction(), r_out.direction());
+
+
+		attenuation = vec3(1);
+
+		return true;
+	}
+	virtual double get_pdf(const vec3& wi, const vec3& wo, const vec3& n) const override {
+		return 1 / (2 * pi);
+	}
+	virtual double get_bsdf(const vec3& wi, const vec3& wo, const vec3& n)const override {
+		return 1;
 	}
 };
 #endif // !MATERIAL
